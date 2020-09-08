dfcd3799ea2551718e13436f4a660fcadb383982
Shivam Mishra <shivammishra7763914693@gmail.com>
2020-09-08 19:19:18 +0530

    server_set_up

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors())
+app.use(express.json())
+app.listen(3000,()=>{
+    console.log('sever started on port 3000');
+})
